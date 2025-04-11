838ce6c582ea81cac5af3f4d93cb2e23d2a9e029
Aric Gardner <aric.gardner@aifi.com>
2025-04-11 15:54:56 -0400

    feat: initialize project with basic vue setup and data file

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Team Balancer</h1>
+    <!-- Components will go here -->
+  </div>
+</template>
+
+<script setup>
+// Script setup for Vue 3 Composition API
+</script>
+
+<style>
+/* Global styles can go here */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
